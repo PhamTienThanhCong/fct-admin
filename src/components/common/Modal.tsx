3ac971768ea58d9bfccd8c13731eda504e59ee2a
mien1234 <vuthimienhongthai@gmail.com>
2023-09-30 01:18:02 +0700

    feature/add/user

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import { Modal, Form, Input, Button } from "antd";
+
+export interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
+
+interface UserModalProps {
+  visible: boolean;
+  onCancel: () => void;
+  onAction: (values: DataType) => void;
+  mode: "add" | "edit" | "delete";
+  userData?: DataType;
+}
+
+const UserModal: React.FC<UserModalProps> = ({
+  visible,
+  onCancel,
+  onAction,
+  mode,
+  userData,
+}) => {
+  const [form] = Form.useForm();
+  const [formData, setFormData] = useState<DataType>({
+    key: userData?.key || '',
+    name: "",
+    age: 0,
+    address: "",
+  });
+
+  // Lưu thông tin người dùng ban đầu
+  const [initialUserData, setInitialUserData] = useState<DataType | undefined>(
+    userData
+  );
+
+  useEffect(() => {
+    // Khi modal mở và chế độ là "edit," sử dụng thông tin người dùng ban đầu để khởi tạo formData
+    if (visible && mode === "edit" && initialUserData) {
+      setFormData(initialUserData);
+      form.setFieldsValue(initialUserData);
+    } else {
+      // Khi modal đóng hoặc chế độ không phải "edit," reset formData và form
+      setFormData({
+        key: userData?.key || '',
+        name: "",
+        age: 0,
+        address: "",
+      });
+      form.resetFields();
+    }
+  }, [visible, mode, initialUserData, form, userData]);
+
+  const handleAddUser = () => {
+    form.validateFields().then(() => {
+      onCancel();
+      onAction(formData);
+    });
+  };
+
+  return (
+    <Modal
+      title={mode === "add" ? "Thêm Người Dùng" : "Sửa Người Dùng"}
+      visible={visible}
+      onOk={handleAddUser}
+      onCancel={onCancel}
+    >
+      <Form form={form} name="userForm" initialValues={formData}>
+        <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+          <Input
+            value={formData.name}
+            onChange={(e) =>
+              setFormData({ ...formData, name: e.target.value })
+            }
+          />
+        </Form.Item>
+        <Form.Item name="age" label="Age">
+          <Input
+            type="number"
+            value={formData.age}
+            onChange={(e) =>
+              setFormData({ ...formData, age: parseInt(e.target.value) || 0 })
+            }
+          />
+        </Form.Item>
+        <Form.Item name="address" label="Address">
+          <Input
+            value={formData.address}
+            onChange={(e) =>
+              setFormData({ ...formData, address: e.target.value })
+            }
+          />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default UserModal;
--- src/modules/user/ListUser.tsx
+++ src/modules/user/ListUser.tsx
@@ -1,50 +1,48 @@
-import { Button, Table } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
-import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Button, Table, Modal, message } from "antd";
+import type { ColumnsType } from "antd/es/table";
+import UserModal, { DataType } from "../../components/common/Modal";
 
-interface DataType {
-  key: React.Key;
-  name: string;
-  age: number;
-  address: string;
-}
-
-const ListUser = () => {
-  const navigate = useNavigate();
+const ListUser: React.FC = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalMode, setModalMode] = useState<"add" | "edit" | "delete">("add");
   const [pageNumber, setPageNumber] = useState<number>(1);
   const [pageSize, setPageSize] = useState<number>(10);
-  // const dispatch = useDispatch<any>();
+  const [dataSource, setDataSource] = useState<DataType[]>([]);
+  const [selectedUser, setSelectedUser] = useState<DataType | null>(null);
 
   const columns: ColumnsType<DataType> = [
     {
-      title: 'Name',
-      dataIndex: 'name',
+      title: "Name",
+      dataIndex: "name",
       sorter: true,
-      width: '25%', 
+      width: "25%",
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
+      title: "Age",
+      dataIndex: "age",
       sorter: false,
-      width: '25%', 
+      width: "25%",
     },
     {
-      title: 'Address',
-      dataIndex: 'address',
-      width: '25%',
+      title: "Address",
+      dataIndex: "address",
+      width: "25%",
     },
     {
-      title: 'Action',
-      dataIndex: 'action',
-      width: '25%', 
+      title: "Action",
+      dataIndex: "action",
+      width: "25%",
       render: (_, record) => (
-        <span className='action-btn-user'>
-          <Button style={{marginRight:'10px'}} type='primary' onClick={() => handleEdit(record.key)}>
+        <span className="action-btn-user">
+          <Button
+            style={{ marginRight: "10px" }}
+            type="primary"
+            onClick={() => handleEdit(record)}
+          >
             Sửa
           </Button>
-          <Button type='default' onClick={() => handleDelete(record.key)}>
+          <Button type="default" onClick={() => showDeleteConfirm(record)}>
             Xóa
           </Button>
         </span>
@@ -52,55 +50,113 @@ const ListUser = () => {
     },
   ];
 
-  const data: DataType[] = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-  ];
+  useEffect(() => {
+    // Khôi phục danh sách người dùng từ localStorage khi trang được nạp lại
+    const storedData = localStorage.getItem("userList");
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      setDataSource(parsedData);
+    }
+  }, []);
 
-  // Xử lý sự kiện khi nhấn nút Sửa
-  const handleEdit = (key: React.Key) => {
-    console.log(`Sửa người dùng có key: ${key}`);
+  const handleEdit = (user: DataType) => {
+    setModalMode("edit");
+    setSelectedUser(user);
+    setIsModalVisible(true);
   };
 
-  // Xử lý sự kiện khi nhấn nút Xóa
-  const handleDelete = (key: React.Key) => {
-    console.log(`Xóa người dùng có key: ${key}`);
+  const showDeleteConfirm = (user: DataType) => {
+    Modal.confirm({
+      title: "Xác nhận xóa",
+      content: `Bạn có muốn xóa người dùng "${user.name}" không?`,
+      okText: "Xóa",
+      okType: "danger",
+      cancelText: "Hủy",
+      onOk() {
+        handleDelete(user);
+      },
+    });
   };
+
+  const handleDelete = (user: DataType) => {
+    try {
+      const updatedDataSource = dataSource.filter((item) => item.key !== user.key);
+      localStorage.setItem("userList", JSON.stringify(updatedDataSource));
+      setDataSource(updatedDataSource);
+      message.success("Xóa người dùng thành công");
+    } catch (error) {
+      message.error("Xóa người dùng thất bại");
+    }
+  };
+
+  const handleAddUser = () => {
+    setModalMode("add");
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleSaveUser = (values: DataType) => {
+    if (modalMode === "add") {
+      // Thêm người dùng mới vào mảng dataSource
+      const newUser = { ...values, key: dataSource.length + 1 };
+      const updatedDataSource = [...dataSource, newUser];
+      // Lưu danh sách người dùng vào localStorage
+      localStorage.setItem("userList", JSON.stringify(updatedDataSource));
+      // Cập nhật dataSource và đóng modal
+      setDataSource(updatedDataSource);
+      setIsModalVisible(false);
+      message.success("Thêm người dùng thành công");
+    } else if (modalMode === "edit") {
+      // Sửa người dùng
+      const updatedDataSource = dataSource.map((item) =>
+        item.key === values.key ? { ...item, ...values } : item
+      );
+      // Lưu danh sách người dùng vào localStorage
+      localStorage.setItem("userList", JSON.stringify(updatedDataSource));
+      // Cập nhật dataSource và đóng modal
+      setDataSource(updatedDataSource);
+      setIsModalVisible(false);
+      message.success("Sửa người dùng thành công");
+    }
+  };
+  
 
   return (
-    <div className='content-userList'>
-      <div className='title-table-user'>
+    <div className="content-userList">
+      <div className="title-table-user">
         <h2>Danh sách người dùng</h2>
-        <Button type='primary' onClick={() => navigate('/listUser')}>
+        <Button type="primary" onClick={handleAddUser}>
           Thêm Người Dùng
         </Button>
       </div>
-      <div className='main-container'>
+      <div className="main-container">
         <Table
-          dataSource={data}
+          dataSource={dataSource}
           columns={columns}
           pagination={{
             current: pageNumber,
             pageSize: pageSize,
-            total: data.length,
+            total: dataSource.length,
             showSizeChanger: true,
           }}
-          onChange={(pagination, filters, sorter) => {
+          onChange={(pagination) => {
             setPageNumber(pagination.current || 1);
             setPageSize(pagination.pageSize || 10);
           }}
         />
       </div>
+      {isModalVisible && (
+        <UserModal
+          visible={isModalVisible}
+          onCancel={handleCancel}
+          onAction={handleSaveUser}
+          mode={modalMode}
+          userData={selectedUser || undefined}
+        />
+      )}
     </div>
   );
 };
