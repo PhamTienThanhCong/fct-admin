64266fdb189db1acb146c7873361fa5dd04fbbe0
mien1234 <vuthimienhongthai@gmail.com>
2023-09-30 10:31:47 +0700

    save

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import Topage from './modules/top/topPage';
+import Login from './modules/auth/Login';
 
 function App() {
   return (
     <Router>
       <Provider store={store}>
-        <Topage/>
+        {/* <Topage/> */}
+        <Login/>
       </Provider>
     </Router>
   );
