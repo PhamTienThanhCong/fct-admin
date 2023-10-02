62d80d02b6f21be6d56828ea967731dfd0838bd5
congphamtienthanh <congphamtienthanh@gmail.com>
2023-10-02 23:22:29 +0700

    deon

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./assets/App.css";
+// import "./assets/App.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
