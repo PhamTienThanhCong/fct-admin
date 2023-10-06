4f1a0bb11591bd4fce25fc93d6cc84aaac22f85f
mien1234 <vuthimienhongthai@gmail.com>
2023-10-06 16:11:57 +0700

    feature/dashbroad

--- src/modules/dashBroad/DashBroad.tsx
+++ src/modules/dashBroad/DashBroad.tsx
@@ -1,50 +1,72 @@
-import { DollarCircleOutlined, ShoppingCartOutlined, ShoppingOutlined, UserOutlined } from "@ant-design/icons";
-import { Col, Row, Space, Typography } from "antd";
-import '../dashBroad/DashBroad.scss';
+import React from "react";
+import {
+  DollarCircleOutlined,
+  ShoppingCartOutlined,
+  ShoppingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Col, Row } from "antd";
+import "./DashBroad.scss"; // Đảm bảo bạn import CSS đúng cách
 
 function DashBroad() {
-    return (
-       <div>
-            <Row gutter={16}>
-                <Col md={6}>
-                    <div className="dashboard-box">
-                        <div className="icon-container">
-                            <ShoppingCartOutlined style={{ color: "#FF5733" }} />
-                        </div>
-                        <span className="dashboard-text">Order</span>
-                        <span className="dashboard-number">12345</span>
-                    </div>
-                </Col>
-                <Col md={6}>
-                    <div className="dashboard-box">
-                        <div className="icon-container">
-                            <ShoppingOutlined style={{ color: "#3498db" }} />
-                        </div>
-                        <span className="dashboard-text">Order</span>
-                        <span className="dashboard-number">12345</span>
-                    </div>
-                </Col>
-                <Col md={6}>
-                    <div className="dashboard-box">
-                        <div className="icon-container">
-                            <UserOutlined style={{ color: "#9b59b6" }} />
-                        </div>
-                        <span className="dashboard-text">Order</span>
-                        <span className="dashboard-number">12345</span>
-                    </div>
-                </Col>
-                <Col md={6}>
-                    <div className="dashboard-box">
-                        <div className="icon-container">
-                            <DollarCircleOutlined style={{ color: "#e74c3c", fontSize: "20px" }} />
-                        </div>
-                        <span className="dashboard-text">Order</span>
-                        <span className="dashboard-number">12345</span>
-                    </div>
-                </Col>
-            </Row>
-       </div>
-    );
+  return (
+    <div>
+      <Row gutter={16}>
+        <Col md={6}>
+          <div className="dashboard-box">
+            <div className="icon-container" style={{ backgroundColor: "#4cd137" }}>
+              <span>
+                <ShoppingCartOutlined/>
+              </span>
+            </div>
+            <div className="dashbroad-title">
+              <p className="dashboard-text">Order</p>
+              <span className="dashboard-number">12345</span>
+            </div>
+          </div>
+        </Col>
+        <Col md={6}>
+          <div className="dashboard-box">
+            <div className="icon-container" style={{ backgroundColor: "#3498db" }}>
+              <span>
+                <ShoppingOutlined/>
+              </span>
+            </div>
+            <div className="dashbroad-title">
+              <p className="dashboard-text">Order</p>
+              <span className="dashboard-number">12345</span>
+            </div>
+          </div>
+        </Col>
+        <Col md={6}>
+          <div className="dashboard-box">
+            <div className="icon-container" style={{ backgroundColor: "#9b59b6" }}>
+              <span>
+                <UserOutlined/>
+              </span>
+            </div>
+            <div className="dashbroad-title">
+              <p className="dashboard-text">Order</p>
+              <span className="dashboard-number">12345</span>
+            </div>
+          </div>
+        </Col>
+        <Col md={6}>
+          <div className="dashboard-box">
+            <div className="icon-container" style={{ backgroundColor: "#e74c3c" }}>
+              <span>
+                <DollarCircleOutlined style={{ color: "#fff", fontSize: "20px" }} />
+              </span>
+            </div>
+            <div className="dashbroad-title">
+              <p className="dashboard-text">Order</p>
+              <span className="dashboard-number">12345</span>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default DashBroad;
